a2f157fa472ed9c5c877265b7ca675e040ee804e
ShowEventXCDMG <95542265+ShowEventXCDMG@users.noreply.github.com>
2021-12-04 22:17:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-<?php
-$f = "visit.php";
-if(!file_exists($f)){
-	touch($f);
-	$handle =  fopen($f, "w" ) ;
-	fwrite($handle,0) ;
-	fclose ($handle);
-}
- 
-include('qrlib.php'); 
 
-function getUsernameFromEmail($email) {
-	$find = '@';
-	$pos = strpos($email, $find);
-	$username = substr($email, 0, $pos);
-	return $username;
-}
-
-if(isset($_POST['submit']) ) {
-	$tempDir = 'temp/'; 
-	$email = $_POST['mail'];
-	$subject =  $_POST['subject'];
-	$filename = getUsernameFromEmail($email);
-	$body =  $_POST['msg'];
-	$codeContents = 'mail:'.$email.
-	               'Nom='.urlencode($subject).
-	               'Contact='.urlencode($body); 
-	QRcode::png($codeContents, $tempDir.''.$filename.'.png', QR_ECLEVEL_L, 5);
-}
-?>
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
@@ -68,11 +39,7 @@ if(isset($_POST['submit']) ) {
 					</div>
 				</form>
 			</div>
-			<?php
-			if(!isset($filename)){
-				$filename = "author";
-			}
-			?>
+			
 			<div class="qr-field">
 				<h2 style="text-align:center; color:white;">QR Code Result: </h2>
 				<center>
